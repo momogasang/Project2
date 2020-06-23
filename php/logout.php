fd464f6f209f872354fa8b5f6df37f8983574531
19302010055@fudan.edu.cn <19302010055@fudan.edu.cn>
2020-06-23 14:53:21 +0800

    pj

--- php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// include 'mysqli.php';
-
-//设置cookie过期，跳转至登陆
-if(isset($_COOKIE['username'])){
-    setcookie("username","",time()-86400);
-    Header("Location: /index.html");
-    exit; 
-}
